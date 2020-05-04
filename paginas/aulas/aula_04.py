4986da344ed99a37efbd56764e1337d32074e794
dunossauro <mendesxeduardo@gmail.com>
2020-05-04 06:50:24 -0300

    fix pep-8

--- paginas/aulas/aula_04.py
+++ paginas/aulas/aula_04.py
@@ -4,7 +4,9 @@ from scripts.links import anchor_in_list
 
 lista_exercicios = html.UL()
 for n in range(1, 11):
-    lista_exercicios <= anchor_in_list(f'Exercício {n}', f'exercicio_0{n}.html')
+    lista_exercicios <= anchor_in_list(
+        f'Exercício {n}', f'exercicio_0{n}.html'
+    )
 
 
 lista_aulas = html.UL()
