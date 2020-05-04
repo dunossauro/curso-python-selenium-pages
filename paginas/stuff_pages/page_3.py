17980af26ce35bdd956599f372827c40187223cb
dunossauro <mendesxeduardo@gmail.com>
2020-05-04 06:48:15 -0300

    FIX TYPO gramática

--- paginas/stuff_pages/page_3.py
+++ paginas/stuff_pages/page_3.py
@@ -6,7 +6,7 @@ storage['local'] = 'Página 3'
 
 
 document['main'] <= html.H1('Você está na página 3')
-document['main'] <= html.P('Responda ao certo ou o diabão vai te pegar')
+document['main'] <= html.P('Responda certo ou o diabão vai te pegar')
 document['main'] <= html.P('Qual o final da URL dessa página?')
 document['main'] <= anchor_in_list('page_3.html', 'page_4.html')
 document['main'] <= anchor_in_list('bage_3.html', 'diabao.html')
