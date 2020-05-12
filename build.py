d1045bcde62423b3f8abd3d8f8d7fe76756d113c
dunossauro <mendesxeduardo@gmail.com>
2020-05-12 07:55:21 -0300

    HOTFIX exercício 05

--- build.py
+++ build.py
@@ -71,6 +71,16 @@ make_page(
     },
 )
 
+make_page(
+    'exercicio_05',
+    {
+        'title': 'Exercício 05',
+        'type': 'exercicios',
+        'path': 'exercicios',
+        'brython_file': 'exercicio_05',
+    },
+)
+
 # stuff pages
 from os import listdir
 path = 'paginas/stuff_pages'
