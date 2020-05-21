be2f87d478a463d985b2d839ab8dc3d97c267afd
dunossauro <mendesxeduardo@gmail.com>
2020-05-21 00:31:21 -0300

    Simplificando o build

--- build.py
+++ build.py
@@ -4,16 +4,6 @@ from template_engine import clean_and_copy, make_page, build_path
 
 clean_and_copy()
 
-make_page(
-    'index',
-    {
-        'title': 'Olar',
-        'type': 'aulas',
-        'path': 'aulas',
-        'brython_file': 'index',
-    },
-)
-
 page_paths = ['paginas/aulas', 'paginas/stuff_pages', 'paginas/exercicios']
 
 for path in page_paths:
