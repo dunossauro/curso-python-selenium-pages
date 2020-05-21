21b776a848bb64c8623b1ab33fcefbddc2be39e1
dunossauro <mendesxeduardo@gmail.com>
2020-05-21 00:01:14 -0300

    Ajustando a página do index

---
+++ paginas/stuff_pages/index.py
@@ -0,0 +1,20 @@
+from browser import html, document
+from scripts.links import anchor_in_list
+
+
+lista_exercicios = html.UL()
+for n in range(1, 8):
+    lista_exercicios <= anchor_in_list(
+        f'Exercício {n}', f'exercicio_0{n}.html'
+    )
+
+
+lista_aulas = html.UL()
+for n in range(3, 8):
+    lista_aulas <= anchor_in_list(f'Aula {n}', f'aula_0{n}.html')
+
+document['aside'] <= html.H1('Lista de Aulas')
+document['aside'] <= lista_aulas
+
+document['main'] <= html.H1('Lista de exercícios')
+document['main'] <= lista_exercicios
